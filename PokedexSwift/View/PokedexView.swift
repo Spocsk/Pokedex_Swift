5217fb1c12161d5f7b67da7c457ec495f0816c24
Dylan COUTO DE OLIVEIRA <dcoutodeoliveira@normandiewebschool.fr>
2021-12-01 00:41:44 +0100

    Pokemon Cell added

---
+++ PokedexSwift/View/PokedexView.swift
@@ -0,0 +1,26 @@
+//
+//  PokedexView.swift
+//  PokedexSwift
+//
+//  Created by Dylan COUTO DE OLIVEIRA on 01/12/2021.
+//
+
+import SwiftUI
+
+struct PokedexView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid {
+                     
+                }
+            }
+        }
+    }
+}
+
+struct PokedexView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokedexView()
+    }
+}
