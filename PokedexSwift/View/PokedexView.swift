27cc48fa18b3505981842fedfb58ed326f41037b
Dylan COUTO DE OLIVEIRA <dcoutodeoliveira@normandiewebschool.fr>
2021-12-01 00:51:08 +0100

    Pokedex view added

--- PokedexSwift/View/PokedexView.swift
+++ PokedexSwift/View/PokedexView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct PokedexView: View {
+    private let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
     var body: some View {
         NavigationView {
             ScrollView {
-                LazyVGrid {
-                     
+                LazyVGrid(columns: gridItems, spacing: 20){
+                    ForEach(0..<151) { _ in
+                        PokemonCell()
+                    }
                 }
             }
+            .navigationTitle("Pokedex")
         }
     }
 }
