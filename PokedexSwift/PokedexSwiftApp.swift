4eb0ba1d135871e4a64e1ff45311d0db9ccc5088
Dylan COUTO DE OLIVEIRA <dcoutodeoliveira@normandiewebschool.fr>
2021-12-01 00:15:29 +0100

    Project initialize

---
+++ PokedexSwift/PokedexSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftApp.swift
+//  PokedexSwift
+//
+//  Created by Dylan COUTO DE OLIVEIRA on 01/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
